156ced6ba79caa499165daf5be4a86003d5e942c
Michael <mpatterson@doubleencore.com>
2015-09-02 19:41:01 -0600

    initial commit
    Basic play button over a movie poster passes a url thru to a blank activity with a single surface view

---
+++ app/src/main/java/com/doubleencore/mpatterson/ui/MainActivity.java
@@ -0,0 +1,31 @@
+package com.doubleencore.mpatterson.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.doubleencore.mpatterson.R;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ImageView mPlayButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPlayButton = (ImageView) findViewById(R.id.play_button);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VideoPlayerActivity.class);
+                intent.putExtra(VideoPlayerActivity.EXTRA_URL, getString(R.string.video_url));
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/doubleencore/mpatterson/ui/VideoPlayerActivity.java
@@ -0,0 +1,45 @@
+package com.doubleencore.mpatterson.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.doubleencore.mpatterson.R;
+
+/**
+ * Created by michael on 9/2/15.
+ */
+public class VideoPlayerActivity extends Activity {
+    private static final String TAG = VideoPlayerActivity.class.getSimpleName();
+
+    public static final String EXTRA_URL = "extra_url";
+
+    private SurfaceView mPlayerView;
+
+    private String mVideoUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_player);
+        mPlayerView = (SurfaceView) findViewById(R.id.player_view);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_URL)) {
+            mVideoUrl = intent.getStringExtra(EXTRA_URL);
+        } else {
+            showError();
+        }
+    }
+
+
+
+
+    private void showError() {
+        Toast.makeText(this, R.string.video_player_error, Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+}
