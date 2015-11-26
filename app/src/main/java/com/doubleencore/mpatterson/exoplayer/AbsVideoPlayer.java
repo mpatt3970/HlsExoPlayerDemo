13ec898ef807b12864a3a9c03010990b9556e58f
Michael <mpatterson@doubleencore.com>
2015-11-26 13:49:54 -0500

    added some vars I'll need soon

--- app/src/main/java/com/doubleencore/mpatterson/exoplayer/AbsVideoPlayer.java
+++ app/src/main/java/com/doubleencore/mpatterson/exoplayer/AbsVideoPlayer.java
@@ -1,6 +1,7 @@
 package com.doubleencore.mpatterson.exoplayer;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.SurfaceHolder;
@@ -8,13 +9,22 @@ import android.view.SurfaceView;
 import android.widget.FrameLayout;
 
 import com.google.android.exoplayer.AspectRatioFrameLayout;
+import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.audio.AudioCapabilities;
+import com.google.android.exoplayer.util.PlayerControl;
 
 /**
  * Created by michael on 11/26/15.
+ * This class handles underlying business to set up the exoplayer
  */
 public abstract class AbsVideoPlayer extends AbsVideoPlayerImpl {
 
     private SurfaceHolder mSurfaceHolder;
+    private HlsRendererBuilder mBuilder;
+    private ExoPlayer mExoPlayer;
+    protected PlayerControl mPlayerController;
+    private AudioCapabilities mAudioCapabilitiesReceiver;
+    private Handler mHandler;
 
     public AbsVideoPlayer(Context context) {
         this(context, null);
