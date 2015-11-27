bd80690bd9ebb1e3e48e8595ecb3d151adef8ef3
Michael <mpatterson@doubleencore.com>
2015-11-27 12:06:48 -0500

    added the layout for controls
    for simplicity, just fit to system windows and dont worry about changing system ui visibility

--- app/src/main/java/com/doubleencore/mpatterson/ui/VideoControlsView.java
+++ app/src/main/java/com/doubleencore/mpatterson/ui/VideoControlsView.java
@@ -1,7 +1,27 @@
 package com.doubleencore.mpatterson.ui;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+
+import com.doubleencore.mpatterson.R;
+
 /**
  * Created by michael on 11/27/15.
  */
-public class VideoControlsView {
+public class VideoControlsView extends RelativeLayout {
+
+    public VideoControlsView(Context context) {
+        this(context, null);
+    }
+
+    public VideoControlsView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public VideoControlsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        LayoutInflater.from(context).inflate(R.layout.video_controls, this, true);
+    }
 }
